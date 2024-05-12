0af144d352f6b2ecf40138ac8518e6f99d81804a
justinjdev <justin@justinj.dev>
2024-05-12 09:16:03 -0500

    lesson 2

---
+++ src/lib/components/examples/TwoWayBinding.svelte
@@ -0,0 +1,12 @@
+<script>
+	import ExampleWrapper from './ExampleWrapper.svelte';
+
+	let name = ''; // State variable
+</script>
+
+<ExampleWrapper>
+	<input type="text" bind:value={name} placeholder="Enter your name" />
+	<!-- `value` of the input is bound to the `name` variable -->
+	<p>Hello, {name}!</p>
+	<!-- Any changes in the input will automatically update here and vice versa -->
+</ExampleWrapper>

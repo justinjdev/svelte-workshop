c8c4fdee95e0f905eb3c4c6a41c3e3ebc0363730
Justin Jones <justinj@ziprecruiter.com>
2024-06-03 10:08:14 -0700

    sample data

--- src/routes/(everything_else)/exercise/f1/data.ts
+++ src/routes/(everything_else)/exercise/f1/data.ts
@@ -4,10 +4,18 @@ import type { Driver, Race, Result, Team } from '$lib/server/db/types';
  * FINDTHEDATA
  */
 
-export const drivers: Driver[] = [];
+export const drivers: Driver[] = [
+    { id: 1, name: 'Lando Norris',code: 'NOR', number: 4, teamId: 1, points: 100, nationality: 'UK', dateOfBirth: new Date() }
+];
 
-export const teams: Team[] = [];
+export const teams: Team[] = [
+    { id: 1, name: 'McLaren', base: 'Woking, UK' }
+];
 
-export const results: Result[] = [];
+export const races: Race[] = [
+    { id: 1, trackName: 'Bahrain International Circuit', raceDate: new Date('2024-03-03'), country: 'Bahrain', isSprintRace: false, fastestLap: false }
+];
 
-export const races: Race[] = [];
+export const results: Result[] = [
+    { driverCode: 'VER', raceId: 1, finishingPosition: 1, pointsScored: 25, dnf: false, fastestLap: false }
+];
